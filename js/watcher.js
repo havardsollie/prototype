c65870d68aecb24d09f102c44392489656cf4406
havardsollie <79274753+havardsollie@users.noreply.github.com>
2021-12-17 16:21:09 +0100

    watchlist

--- js/watcher.js
+++ js/watcher.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://www.squareeyes.one/wp-json/wc/store/products/";
+const baseUrl = "https://www.squareeyes.one/wp-json/wc/store/products/?featured=true";
 const dataSection = document.querySelector(".main3");
 
 async function getData(url) {
@@ -6,12 +6,13 @@ async function getData(url) {
     const data = await response.json();
 
     data.forEach(function(data) {
-        for (let i = 0; i < data.length; i++)
         dataSection.innerHTML += `
+        <div class="watchlist">
             <div class="product">
                 <h2>${data.name}</h2>
                 <img src="${data.images[0].src}" alt="${data.name}"
-        </div>`     
+        </div>
+        </div>`
     })
 }
 
